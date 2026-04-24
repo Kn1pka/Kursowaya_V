42105169ec1301722dcaed7770027bfae970f459
Knjpka <knjpka@gmail.com>
2026-04-24 17:39:30 +0300

    Добавьте файлы проекта.

---
+++ Kursowaya_V/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Kursowaya_V
+{
+    partial class Form1 : System.Windows.Forms.Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.difficulty4x4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.difficulty8x8ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.difficulty10x10ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelStats = new System.Windows.Forms.Panel();
+            this.lblBestMoves = new System.Windows.Forms.Label();
+            this.lblBestTime = new System.Windows.Forms.Label();
+            this.lblMoves = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblDifficulty = new System.Windows.Forms.Label();
+            this.panelGameArea = new System.Windows.Forms.Panel();
+            this.tableGrid = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.panelStats.SuspendLayout();
+            this.panelGameArea.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1000, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.difficulty4x4ToolStripMenuItem,
+            this.difficulty8x8ToolStripMenuItem,
+            this.difficulty10x10ToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "Игра";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "Новая игра";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // difficulty4x4ToolStripMenuItem
+            // 
+            this.difficulty4x4ToolStripMenuItem.Name = "difficulty4x4ToolStripMenuItem";
+            this.difficulty4x4ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.difficulty4x4ToolStripMenuItem.Text = "Легко (4x4)";
+            // 
+            // difficulty8x8ToolStripMenuItem
+            // 
+            this.difficulty8x8ToolStripMenuItem.Name = "difficulty8x8ToolStripMenuItem";
+            this.difficulty8x8ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.difficulty8x8ToolStripMenuItem.Text = "Средне (8x8)";
+            // 
+            // difficulty10x10ToolStripMenuItem
+            // 
+            this.difficulty10x10ToolStripMenuItem.Name = "difficulty10x10ToolStripMenuItem";
+            this.difficulty10x10ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.difficulty10x10ToolStripMenuItem.Text = "Сложно (10x10)";
+            // 
+            // panelStats
+            // 
+            this.panelStats.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panelStats.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelStats.Controls.Add(this.lblBestMoves);
+            this.panelStats.Controls.Add(this.lblBestTime);
+            this.panelStats.Controls.Add(this.lblMoves);
+            this.panelStats.Controls.Add(this.lblTime);
+            this.panelStats.Controls.Add(this.lblDifficulty);
+            this.panelStats.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelStats.Location = new System.Drawing.Point(0, 24);
+            this.panelStats.Name = "panelStats";
+            this.panelStats.Size = new System.Drawing.Size(1000, 80);
+            this.panelStats.TabIndex = 1;
+            // 
+            // lblMoves
+            // 
+            this.lblMoves.AutoSize = true;
+            this.lblMoves.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMoves.Location = new System.Drawing.Point(400, 10);
+            this.lblMoves.Name = "lblMoves";
+            this.lblMoves.Size = new System.Drawing.Size(80, 17);
+            this.lblMoves.TabIndex = 2;
+            this.lblMoves.Text = "Ходы: 0";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTime.Location = new System.Drawing.Point(600, 10);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(70, 17);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "Время: 0";
+            // 
+            // lblDifficulty
+            // 
+            this.lblDifficulty.AutoSize = true;
+            this.lblDifficulty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDifficulty.Location = new System.Drawing.Point(15, 10);
+            this.lblDifficulty.Name = "lblDifficulty";
+            this.lblDifficulty.Size = new System.Drawing.Size(100, 17);
+            this.lblDifficulty.TabIndex = 0;
+            this.lblDifficulty.Text = "Режим: 4x4";
+            // 
+            // lblBestTime
+            // 
+            this.lblBestTime.AutoSize = true;
+            this.lblBestTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lblBestTime.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblBestTime.Location = new System.Drawing.Point(600, 40);
+            this.lblBestTime.Name = "lblBestTime";
+            this.lblBestTime.Size = new System.Drawing.Size(105, 15);
+            this.lblBestTime.TabIndex = 4;
+            this.lblBestTime.Text = "Рекорд: -- сек";
+            // 
+            // lblBestMoves
+            // 
+            this.lblBestMoves.AutoSize = true;
+            this.lblBestMoves.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lblBestMoves.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblBestMoves.Location = new System.Drawing.Point(400, 40);
+            this.lblBestMoves.Name = "lblBestMoves";
+            this.lblBestMoves.Size = new System.Drawing.Size(113, 15);
+            this.lblBestMoves.TabIndex = 3;
+            this.lblBestMoves.Text = "Рекорд: -- ходов";
+            // 
+            // panelGameArea
+            // 
+            this.panelGameArea.AutoScroll = true;
+            this.panelGameArea.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panelGameArea.Controls.Add(this.tableGrid);
+            this.panelGameArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelGameArea.Location = new System.Drawing.Point(0, 104);
+            this.panelGameArea.Name = "panelGameArea";
+            this.panelGameArea.Size = new System.Drawing.Size(1000, 596);
+            this.panelGameArea.TabIndex = 2;
+            // 
+            // tableGrid
+            // 
+            this.tableGrid.ColumnCount = 4;
+            this.tableGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableGrid.Location = new System.Drawing.Point(50, 50);
+            this.tableGrid.Name = "tableGrid";
+            this.tableGrid.RowCount = 4;
+            this.tableGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableGrid.Size = new System.Drawing.Size(400, 400);
+            this.tableGrid.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 700);
+            this.Controls.Add(this.panelGameArea);
+            this.Controls.Add(this.panelStats);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Memory Game (Emoji)";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panelStats.ResumeLayout(false);
+            this.panelStats.PerformLayout();
+            this.panelGameArea.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem difficulty4x4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem difficulty8x8ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem difficulty10x10ToolStripMenuItem;
+        private System.Windows.Forms.Panel panelStats;
+        private System.Windows.Forms.Label lblMoves;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblDifficulty;
+        private System.Windows.Forms.Label lblBestTime;
+        private System.Windows.Forms.Label lblBestMoves;
+        private System.Windows.Forms.Panel panelGameArea;
+        private System.Windows.Forms.TableLayoutPanel tableGrid;
+    }
+}
---
+++ Kursowaya_V/Form1.cs
@@ -0,0 +1,295 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace Kursowaya_V
+{
+    public partial class Form1 : Form
+    {
+        // База эмодзи (50 штук, без пропусков)
+        private readonly string[] Emojis = {
+            "🐶", "🐱", "🐭", "🍎", "🐰", "🦊", "🐻", "🍊", "🐨", "🐯",
+            "🦁", "🍋", "🐷", "🐸", "🐵", "🍌", "🍉", "🐦", "🐤", "🦆",
+            "🦅", "🦉", "🦇", "🍇", "🐗", "🐴", "🦄", "🍒", "🐛", "🦋",
+            "🐌", "🐞", "🐜", "🍓", "🦗", "🕷", "🦂", "🍑", "🐍", "🦎",
+            "🦖", "🦕", "🐙", "❤️", "💔", "🦞", "🦀", "🐡", "🐠", "🍍"
+        };
+
+        private int gridSize = 4; // Текущий размер сетки (4, 8 или 10)
+        private int moveCount = 0; // Счётчик ходов
+        private int seconds = 0; // Счётчик секунд
+        private Timer? gameTimer; // Таймер игры
+        private List<Button> flippedCards = new List<Button>(); // Список открытых карт
+        private int matchedPairs = 0; // Количество найденных пар
+        private bool isPaused = false; // Флаг паузы
+
+        // Переменные для рекордов
+        private int bestTime = 0;    // 0 = рекорд ещё не установлен
+        private int bestMoves = 0;   // 0 = рекорд ещё не установлен
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Добавляем кнопку "Пауза" программно в меню
+            ToolStripMenuItem pauseItem = new ToolStripMenuItem("⏸ Пауза");
+            pauseItem.Name = "pauseToolStripMenuItem";
+            pauseItem.Click += PauseGame;
+            fileToolStripMenuItem.DropDownItems.Insert(2, pauseItem);
+
+            // Настройка таймера
+            gameTimer = new Timer();
+            gameTimer.Interval = 1000; // 1 секунда
+            gameTimer.Tick += GameTimer_Tick;
+
+            // Привязка событий меню
+            newGameToolStripMenuItem.Click += (s, e) => StartGame(gridSize);
+            difficulty4x4ToolStripMenuItem.Click += (s, e) => { gridSize = 4; StartGame(4); };
+            difficulty8x8ToolStripMenuItem.Click += (s, e) => { gridSize = 8; StartGame(8); };
+            difficulty10x10ToolStripMenuItem.Click += (s, e) => { gridSize = 10; StartGame(10); };
+
+            StartGame(4); // Запуск с уровнем 4x4 по умолчанию
+        }
+
+        // Метод запуска/перезапуска игры
+        private void StartGame(int size)
+        {
+            gridSize = size;
+            matchedPairs = 0;
+            moveCount = 0;
+            seconds = 0;
+            isPaused = false;
+            flippedCards.Clear();
+
+            // Сброс интерфейса
+            lblMoves.Text = "Ходы: 0";
+            lblTime.Text = "Время: 00";
+            lblDifficulty.Text = $"Режим: {size}x{size}";
+
+            // Сброс кнопки паузы
+            ToolStripItem? pauseBtn = fileToolStripMenuItem.DropDownItems["pauseToolStripMenuItem"];
+            if (pauseBtn != null) pauseBtn.Text = "⏸ Пауза";
+
+            // Перезапуск таймера
+            gameTimer?.Stop();
+            gameTimer?.Start();
+
+            BuildGrid(size); // Построение игрового поля
+            ToggleFieldLock(false); // Разблокировка поля
+        }
+
+        // Метод построения игрового поля
+        private void BuildGrid(int size)
+        {
+            // Очистка старой сетки
+            tableGrid.Controls.Clear();
+            tableGrid.ColumnStyles.Clear();
+            tableGrid.RowStyles.Clear();
+
+            // Настройка количества строк и столбцов
+            tableGrid.ColumnCount = size;
+            tableGrid.RowCount = size;
+
+            // Расчёт размера ячейки (уменьшаем для больших сеток)
+            int cellSize = size == 4 ? 100 : (size == 8 ? 70 : 60);
+
+            // Добавление стилей для равномерного распределения (Percent вместо Absolute!)
+            for (int i = 0; i < size; i++)
+            {
+                tableGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F / size));
+                tableGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 100F / size));
+            }
+
+            // УБРАНО +50 — теперь размер ТОЧНО равен size * cellSize (квадраты не растягиваются)
+            tableGrid.Size = new Size(size * cellSize, size * cellSize);
+
+            // Центрирование поля в панели
+            int startX = (panelGameArea.Width - tableGrid.Width) / 2;
+            int startY = (panelGameArea.Height - tableGrid.Height) / 2;
+
+            if (startX < 10) startX = 10;
+            if (startY < 10) startY = 10;
+
+            tableGrid.Location = new Point(startX, startY);
+
+            // Генерация пар карт
+            int totalCards = size * size;
+            int pairsNeeded = totalCards / 2;
+
+            List<string> cardValues = new List<string>();
+            for (int i = 0; i < pairsNeeded; i++)
+            {
+                string emoji = Emojis[i % Emojis.Length]; // Зацикливаем, если эмодзи не хватит
+                cardValues.Add(emoji);
+                cardValues.Add(emoji);
+            }
+
+            // Перемешивание карт (алгоритм Фишера-Йейтса)
+            Shuffle(cardValues);
+
+            // Создание кнопок-карт
+            foreach (string emoji in cardValues)
+            {
+                Button btn = new Button();
+                btn.Text = ""; // Изначально карта скрыта
+                btn.Tag = emoji; // В Tag храним эмодзи
+
+                // УМЕНЬШЕН шрифт: было cellSize/2, стало cellSize/2.5f (эмодзи помещаются)
+                float fontSize = cellSize / 2.5f;
+                btn.Font = new Font("Segoe UI Emoji", fontSize, FontStyle.Bold);
+
+                btn.BackColor = Color.WhiteSmoke;
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.Dock = DockStyle.Fill;
+                btn.Margin = new Padding(3); // Чуть больше отступ между картами
+                btn.Padding = new Padding(0); // Убираем внутренний отступ
+                btn.TextAlign = ContentAlignment.MiddleCenter; // Центрируем эмодзи
+                btn.UseVisualStyleBackColor = false; // Важно для корректного отображения цвета
+
+                btn.Click += Card_Click; // Подписка на клик
+                tableGrid.Controls.Add(btn); // Добавление в сетку
+            }
+        }
+
+        // Обработчик клика по карте (async для неблокирующей паузы)
+        private async void Card_Click(object? sender, EventArgs e)
+        {
+            if (sender is not Button clickedCard) return;
+            if (isPaused) return; // Если пауза — игнорируем клик
+
+            // Игнорируем, если карта уже открыта или найдена
+            if (clickedCard.Text != "" || flippedCards.Contains(clickedCard)) return;
+
+            // Открываем карту: показываем эмодзи
+            clickedCard.Text = clickedCard.Tag?.ToString() ?? "";
+            clickedCard.BackColor = Color.LightYellow;
+            flippedCards.Add(clickedCard);
+
+            // Если открыты 2 карты — проверяем совпадение
+            if (flippedCards.Count == 2)
+            {
+                moveCount++;
+                lblMoves.Text = $"Ходы: {moveCount}";
+
+                if (flippedCards[0].Tag?.ToString() == flippedCards[1].Tag?.ToString())
+                {
+                    // СОВПАДЕНИЕ! Оставляем карты открытыми
+                    flippedCards[0].BackColor = Color.LightGreen;
+                    flippedCards[1].BackColor = Color.LightGreen;
+                    flippedCards.Clear();
+                    matchedPairs++;
+
+                    // Проверка победы
+                    if (matchedPairs == (gridSize * gridSize) / 2)
+                    {
+                        gameTimer?.Stop();
+                        CheckRecords(); // Проверка рекордов
+                        MessageBox.Show(
+                            $"Победа!\nХодов: {moveCount}\nВремя: {seconds} сек.",
+                            "Поздравляем!",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    // НЕ СОВПАЛИ — закрываем через паузу
+                    Button first = flippedCards[0];
+                    Button second = flippedCards[1];
+                    flippedCards.Clear();
+
+                    // Блокируем поле на время анимации
+                    ToggleFieldLock(true);
+
+                    // НЕблокирующая задержка (таймер продолжает идти!)
+                    await Task.Delay(700);
+
+                    // Закрываем карты
+                    first.Text = "";
+                    first.BackColor = Color.WhiteSmoke;
+                    second.Text = "";
+                    second.BackColor = Color.WhiteSmoke;
+
+                    // Разблокируем поле
+                    ToggleFieldLock(false);
+                }
+            }
+        }
+
+        // Проверка и обновление рекордов
+        private void CheckRecords()
+        {
+            // Проверка времени
+            if (bestTime == 0 || seconds < bestTime)
+            {
+                bestTime = seconds;
+                lblBestTime.Text = $"Рекорд: {bestTime} сек";
+            }
+
+            // Проверка ходов
+            if (bestMoves == 0 || moveCount < bestMoves)
+            {
+                bestMoves = moveCount;
+                lblBestMoves.Text = $"Рекорд: {bestMoves} ходов";
+            }
+        }
+
+        // Обработчик кнопки "Пауза"
+        private void PauseGame(object? sender, EventArgs e)
+        {
+            ToolStripMenuItem? item = sender as ToolStripMenuItem;
+            if (item == null) return;
+
+            if (isPaused)
+            {
+                // ВОЗОБНОВИТЬ
+                isPaused = false;
+                gameTimer?.Start();
+                item.Text = "⏸ Пауза";
+                ToggleFieldLock(false);
+            }
+            else
+            {
+                // ПРИОСТАНОВИТЬ
+                isPaused = true;
+                gameTimer?.Stop();
+                item.Text = "▶ Продолжить";
+                ToggleFieldLock(true);
+            }
+        }
+
+        // Вспомогательный метод: блокировка/разблокировка поля
+        private void ToggleFieldLock(bool isLocked)
+        {
+            foreach (Control c in tableGrid.Controls)
+            {
+                if (c is Button b) b.Enabled = !isLocked;
+            }
+        }
+
+        // Обработчик тика таймера
+        private void GameTimer_Tick(object? sender, EventArgs e)
+        {
+            seconds++;
+            lblTime.Text = $"Время: {seconds}";
+        }
+
+        // Перемешивание списка (алгоритм Фишера-Йейтса)
+        private void Shuffle(List<string> list)
+        {
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                string value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
